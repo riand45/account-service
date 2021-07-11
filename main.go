503a8dbf055363fd977a949c99b3708b484551bb
Riand Pratama <riandpratama4570@gmail.com>
2021-07-11 21:57:45 +0700

    account service with go

---
+++ main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"bwastartup/auth"
+	"bwastartup/handler"
+	"bwastartup/helper"
+	"bwastartup/user"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"log"
+	"net/http"
+	"strings"
+)
+
+func main() {
+	dsn := "root:@tcp(127.0.0.1:3306)/bwastartup?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	userRepository := user.NewRepository(db)
+	userService := user.NewService(userRepository)
+	authService := auth.NewService()
+
+
+	userHandler := handler.NewUserHandler(userService, authService)
+
+	router := gin.Default()
+	api := router.Group("/api/v1")
+
+	api.POST("/register", userHandler.RegisterUser)
+	api.POST("/login", userHandler.LoginUser)
+	api.POST("/email_checkers", userHandler.CheckEmailAvailability)
+	api.POST("/upload_avatars", authMiddleware(authService, userService), userHandler.UploadAvatar)
+
+	router.Run()
+}
+
+func authMiddleware(authService auth.Service, userService user.Service) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+
+		if !strings.Contains(authHeader, "Bearer") {
+			response := helper.APIResponse("Unauthorized", http.StatusUnauthorized, "error", nil)
+			c.AbortWithStatusJSON(http.StatusUnauthorized, response)
+			return
+		}
+
+		defaultToken := ""
+		dataToken := strings.Split(authHeader, " ")
+		if len(dataToken) == 2 {
+			defaultToken = dataToken[1]
+		}
+
+		token, err := authService.ValidateToken(defaultToken)
+		if err != nil {
+			response := helper.APIResponse("Unauthorized", http.StatusUnauthorized, "error", nil)
+			c.AbortWithStatusJSON(http.StatusUnauthorized, response)
+			return
+		}
+
+		claim, success := token.Claims.(jwt.MapClaims)
+
+		if !success || !token.Valid {
+			response := helper.APIResponse("Unauthorized", http.StatusUnauthorized, "error", nil)
+			c.AbortWithStatusJSON(http.StatusUnauthorized, response)
+			return
+		}
+
+		userID := int(claim["user_id"].(float64))
+
+		userSuccess, err := userService.GetUserById(userID)
+		if err != nil {
+			response := helper.APIResponse("Unauthorized", http.StatusUnauthorized, "error", nil)
+			c.AbortWithStatusJSON(http.StatusUnauthorized, response)
+			return
+		}
+
+		c.Set("currentUser", userSuccess)
+	}
+}
